fa70dc355ee0e3dc7d617b14ac42adef407eb2ec
GeorgiDzhanavarov <98283978+GeorgiDzhanavarov@users.noreply.github.com>
2023-03-14 20:18:27 +0200

    Create 03-a-2000.sh

---
+++ repo_tasks/03-a-2000.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+head -n 12 /etc/passwd
+head -c 26 /etc/passwd
+head -n -4 /etc/passwd
+tail -n 17 /etc/passwd
+head -n 151 /etc/passwd | tail -n 1 
+head -n 13 /etc/passwd | tail -n 1 | tail -c 4 
