208831801b8162e9de0298b024fafb5127757282
tsvetellina5 <80826282+tsvetellina5@users.noreply.github.com>
2023-03-19 12:16:15 +0200

    Update 03-a-2000.sh
    Why? I think it`s unnecessary. Prove me wrong.

--- repo_tasks/03-a-2000.sh
+++ repo_tasks/03-a-2000.sh
@@ -5,4 +5,4 @@ head -c 26 /etc/passwd
 head -n -4 /etc/passwd
 tail -n 17 /etc/passwd
 head -n 151 /etc/passwd | tail -n 1 
-head -n 13 /etc/passwd | tail -n 1 | tail -c 4 
+head -n 13 /etc/passwd  tail -c 4 
