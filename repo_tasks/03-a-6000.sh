f687f44aa2ee6c28927dd29ed34feb3f3b5749c7
GeorgiDzhanavarov <98283978+GeorgiDzhanavarov@users.noreply.github.com>
2023-03-20 10:08:31 +0200

    Update 03-a-6000.sh

--- repo_tasks/03-a-6000.sh
+++ repo_tasks/03-a-6000.sh
@@ -3,3 +3,35 @@
  cat ~/emp.data | awk 'BEGIN {i = 0} {i += 1} END {print(i)}'
  
  cat ~/emp.data | awk 'NR == 3 {print $0}'
+ 
+ cat ~/emp.data | awk '{print $NF}'
+
+ cat ~/emp.data | awk 'END {print $NF}' 
+ 
+ cat ~/emp.data | awk 'NF > 4 {print $0}'
+ 
+ cat ~/emp.data | awk '$NF > 4 {print $0}'
+ 
+ cat ~/emp.data | awk 'BEGIN {i = 0} {i += NF} END{print i}'
+ 
+ cat ~/emp.data | awk 'BEGIN {i =0} $1=="Beth" {i +=1 } END {i}'
+ 
+ cat ~/emp.data | awk 'BEGIN {i = -1} $3 > i {i = $3;v = $0} END{print i,v}'
+ 
+ cat ~/emp.data | awk 'NF >= 1 {print $0}'
+ 
+ cat ~/emp.data | awk 'length() > 17 {print $0}'
+ 
+ cat ~/emp.data | awk '{print NF,$0}'
+ 
+ cat ~/emp.data | awk '{print $2,$1}'
+ 
+ cat ~/emp.data | awk '{print $2,$1,$3}'
+ 
+ cat ~/emp.data | awk '{print NR,$2,$3}'
+ 
+ cat ~/emp.data | awk '{print $1,$3}'
+ 
+ cat ~/emp.data | awk '{print $2 + $3}'
+ 
+ cat ~/emp.data | awk 'BEGIN{i = 0} { i += $2 + $3} END{print i}'
