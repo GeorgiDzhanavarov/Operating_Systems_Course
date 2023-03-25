dafc9795f5037d97d24cc82d01c8af401506986e
GeorgiDzhanavarov <98283978+GeorgiDzhanavarov@users.noreply.github.com>
2023-03-25 23:21:14 +0200

    Create 03-b-9051.sh

---
+++ repo_tasks/03-b-9051.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cat ~/population.csv | awk -F',' 'BEGIN{i = 0} $3 == 2008 {i += $4} END{print i}'
+
+
+cat ~/population.csv | awk -F',' 'BEGIN{i = 0} $3 == 2016 {i += $4} END{print i}'
