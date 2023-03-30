69742dcbdb3541e4edccccffd45f746b5dd6829c
GeorgiDzhanavarov <98283978+GeorgiDzhanavarov@users.noreply.github.com>
2023-03-30 10:33:54 +0300

    Update 03-b-9500.sh

--- repo_tasks/03-b-9500.sh
+++ repo_tasks/03-b-9500.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cat ~/ssa-input.txt | egrep '' 
-
-#to-do
+cat ~/ssa-input.txt | awk -F' ' '
+  BEGIN {x = "";y = "";z = "";w = ""}
+  {if($0 ~ /Array [A-Z]/) {x = $NF}
+  if($0 ~ /physicaldrive .*/) {y = $NF}
+  if($0 ~ /Current Temperature/) {z = $NF}
+  if($0 ~ /Maximum Temperature/) {w = $NF; printf("%s-%s %d %d \n",x,y,z,w)}
+}'
