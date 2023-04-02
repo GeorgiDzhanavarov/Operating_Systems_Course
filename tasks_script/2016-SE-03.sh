7e72c1e7958a35e060246ce67a65c8b72540c028
GeorgiDzhanavarov <98283978+GeorgiDzhanavarov@users.noreply.github.com>
2023-04-02 22:01:17 +0300

    Create 2016-SE-03.sh

---
+++ tasks_script/2016-SE-03.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+
+if [[ $(whoami) !=  root ]];
+then
+  echo "not root"
+  exit 1
+fi
+
+cat /etc/passwd | while read line; do
+user = $(echo $line | cut -d':' -f1)
+home = $(echo $line | cut -d':' -f6)
+
+
+if [[ -z $home ]]; then
+  echo "$user has no homedir set"
+  continue
+fi
+
+dirperm = $(ls -ld $home | awk '{print $1}')
+
+if [[ $(echo $dirperm | cut -c 3)) != "w" ]]; then
+  echo "$user cannot write in $home"
+fi
+done
